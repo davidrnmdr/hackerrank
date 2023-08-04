4821f04a6e1d915d65d581caf08d6fdb3b54e413
davidrnmdr <108706773+davidrnmdr@users.noreply.github.com>
2023-08-04 16:37:37 -0300

    Add files via upload

---
+++ c/3-max_of_four.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int maximum(int a, int b){
+    if(a > b) return a;
+    else return b;
+}
+
+int max_of_four(int a, int b, int c, int d){
+    int max = maximum(a, b);
+    max = maximum(max, c);
+    max = maximum(max, d);
+    return max;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
